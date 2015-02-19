6d3348273a2f9a7004a6a9aef50232d6bdfcfb09
Craig Paterson <darksavant@gmail.com>
2015-02-19 21:30:31 +0200

    project setup

--- mix.exs
+++ mix.exs
@@ -8,23 +8,20 @@ defmodule Gmail.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [ {:xoauth2, "~> 0.0.1"}, {:httpoison, "~> 0.5"} ]
   end
+
+  defp package do
+    [
+      files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+      contributors: "Craig Paterson",
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/craigp/elixir-gmail"}]
+  end
+
 end
