c59681ef56afdf14ae17a7a5351ce3cdd7d3d6b4
Craig Paterson <craig@widdershins.co.za>
2015-12-03 12:22:55 +0200

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Gmail.Mixfile do
 
   def project do
     [app: :gmail,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.0",
      deps: deps,
      test_coverage: [tool: ExCoveralls],
