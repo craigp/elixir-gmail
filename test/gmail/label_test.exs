ecb04776b811277f325a248c10e0c76ec1b83759
Craig Paterson <craig@widdershins.co.za>
2016-01-05 19:20:57 +0200

    tests for Gmail.Label.list

--- test/gmail/label_test.exs
+++ test/gmail/label_test.exs
@@ -13,23 +13,32 @@ defmodule Gmail.LabelTest do
     label_id = "Label_22"
     label_name = "Cool Label"
     label_type = "user"
+
     label = %{
       "id" => label_id,
       "name" => label_name,
       "type" => label_type
     }
+
+    labels = %{"labels" => [label]}
+
     expected_result = %Gmail.Label{
       id: label_id,
       name: label_name,
       type: label_type
     }
+
+    expected_results = [expected_result]
+
     {:ok, %{
         access_token: access_token,
         access_token_rec: access_token_rec,
         label_id: label_id,
         label_name: label_name,
         label: label,
-        expected_result: expected_result
+        labels: labels,
+        expected_result: expected_result,
+        expected_results: expected_results
       }}
   end
 
@@ -40,9 +49,16 @@ defmodule Gmail.LabelTest do
 
   # end
 
-  # test "lists all labels" do
-
-  # end
+  test "lists all labels", context do
+    with_mock Gmail.HTTP, [ get: fn _at, _url -> { :ok, context[:labels] } end] do
+      with_mock Gmail.OAuth2, [ get_config: fn -> context[:access_token_rec] end ] do
+        {:ok, labels} = Gmail.Label.list
+        assert context[:expected_results] == labels
+        assert called Gmail.OAuth2.get_config
+        assert called Gmail.HTTP.get(context[:access_token], Gmail.Base.base_url <> "users/me/labels")
+      end
+    end
+  end
 
   # test "updates a label" do
 
