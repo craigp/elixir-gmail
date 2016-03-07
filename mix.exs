4ba6d2413d86951062759a4f896a24e0d808e4fc
Craig Paterson <craig@widdershins.co.za>
2016-03-07 09:17:31 +0200

    bumped version now that tests passing again

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Gmail.Mixfile do
 
   def project do
     [app: :gmail,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.2",
      deps: deps,
      test_coverage: [tool: ExCoveralls],
