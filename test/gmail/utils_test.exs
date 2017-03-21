1bef31d751befc304a5db2b42734f55340a557e6
Johan Sim Jian An <johansja@gmail.com>
2017-03-21 18:20:14 +0800

    Add a test.

--- test/gmail/utils_test.exs
+++ test/gmail/utils_test.exs
@@ -12,5 +12,9 @@ defmodule Gmail.UtilsTest do
     assert 101 == Utils.load_config(:thread, :other_pool_size, 101)
   end
 
+  test "load non existent config with a default" do
+    assert 102 == Utils.load_config(:absent, :pool_size, 102)
+  end
+
 end
 
