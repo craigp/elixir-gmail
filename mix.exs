4c44fbc63b194dc4f6f02caec7c6ee6164a3a9a3
Craig Paterson <craig@widdershins.co.za>
2018-05-24 21:13:27 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Gmail.Mixfile do
 
   def project do
     [app: :gmail,
-     version: "0.1.19",
+     version: "0.1.20",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
